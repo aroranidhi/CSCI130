7102f4c271a9af420df753ebfffd2d4d6c042e18
NIDHI ARORA <nidhiarora9@mail.fresnostate.edu>
2016-01-30 09:48:45 +0530

    HW2

---
+++ HW2/variable_type.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+     a := 9
+     b := "hello"
+     c := 1.4
+     
+     fmt.Printf("The type of variable x is %T.\n", a)
+     fmt.Printf("The type of variable y is %T.\n", b)
+     fmt.Printf("The type of variable x is %T.\n", c)
+    
+}
