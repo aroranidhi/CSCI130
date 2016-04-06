c1cde3147fef66d7d5af2c1a33925f0e0963d9e1
NIDHI ARORA <nidhiarora9@mail.fresnostate.edu>
2016-04-06 10:20:33 -0700

    Proj

---
+++ TLS/TLS.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "io"
+    "net/http"
+    "log"
+)
+
+func HelloServer(w http.ResponseWriter, req *http.Request) {
+    io.WriteString(w, "hello, world!\n")
+}
+
+func main() {
+    http.HandleFunc("/hello", HelloServer)
+    err := http.ListenAndServeTLS(":443", "cert.pem", "key.pem", nil)
+    if err != nil {
+        log.Fatal("ListenAndServe: ", err)
+    }
+}
+
